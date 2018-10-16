fdc88158300f0b4fb106ba7458b1051956d38918
Luke Brady <lbrad23105@gmail.com>
2018-10-16 02:19:22 -0400

    Implemented a simple stack.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

---
+++ stack/stack.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int stack_pos = 0;
+
+int post(int *stack, int value) {
+    if(stack_pos < 100) {
+        stack[stack_pos] = value;
+        stack_pos++;
+    } 
+    return 0;
+}
+
+int pop(int *stack) {
+    if(stack_pos != 0) {
+        stack_pos--;
+        stack[stack_pos] = 0;
+    } else {
+        printf("The stack is empty.\n");
+    }
+    return 0;
+}
+
+int main() {
+    int *stack;
+    int stack_len;
+
+    stack = (int *)malloc(100);
+    post(stack, 3);
+    post(stack, 5);
+    pop(stack);
+    pop(stack);
+    pop(stack);
+
+    stack_len = sizeof(stack) / sizeof(int);
+
+    for(int i = 0; i < stack_len; i++) {
+        if(stack[i] != 0) {
+            printf("%d\n", stack[i]);
+        }
+    }
+
+    free(stack);
+}
